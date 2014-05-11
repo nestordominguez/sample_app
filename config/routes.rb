9f8a33d6f17b1611ae09b0bd67bc819607f418ce
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2014-05-10 22:12:14 -0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   
-  get "users/new"
+  resources :users
   match '/signup',    to: 'users#new',              via: 'get'
   match '/contact',   to: 'static_pages#contact',   via: 'get'
   match '/about',     to: 'static_pages#about',     via: 'get'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Oscar Dominguez"
+    email    "oscardom80@hotmail.com"
+    password "magoloco"
+    password_confirmation "magoloco"
+  end
+end
